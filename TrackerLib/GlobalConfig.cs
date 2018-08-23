115155f42db3b1751c265a17d776d0abdbfb3469
Hassan OUZEGDOUH <ouzegdouh.hassan@gmail.com>
2018-08-23 04:04:53 +0200

    Data config files

---
+++ TrackerLib/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLib
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if(database)
+            {
+                // à faire créer connection sql
+            }
+
+            if (textFiles)
+            {
+                // à faire créer connection text
+            }
+        }
+    }
+}
