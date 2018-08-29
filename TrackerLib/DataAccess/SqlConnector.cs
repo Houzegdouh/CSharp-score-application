1116159294069d949ba1dc4076c6f700b27b7098
Hassan OUZEGDOUH <ouzegdouh.hassan@gmail.com>
2018-08-29 04:41:46 +0200

    Insert stored procedure, Prize procedure

--- TrackerLib/DataAccess/SqlConnector.cs
+++ TrackerLib/DataAccess/SqlConnector.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -8,6 +9,11 @@ using TrackerLib.Models;
 
 namespace TrackerLib.DataAccess
 {
+ //   @PlaceNumber int,
+	//@PlaceName nvarchar(50),
+	//@PrizeAmount money,
+ //   @PrizePercentage float,
+	//@id int = 0 output
     class SqlConnector : IDataConnection
     {
         // TODO - Faire réellement enregistrer le prix dans la base de données
@@ -20,10 +26,19 @@ namespace TrackerLib.DataAccess
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
             {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+                
+                return model;
             };
-            model.Id = 1;
-            return model;
         }
     }
 }
