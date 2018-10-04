4307e02e5a1976fea2380f830a2ffe3cd47a2590
Hassan OUZEGDOUH <ouzegdouh.hassan@gmail.com>
2018-10-05 01:05:51 +0200

    Conversion model to file

--- TrackerLib/DataAccess/TextConnector.cs
+++ TrackerLib/DataAccess/TextConnector.cs
@@ -17,6 +17,12 @@ namespace TrackerLib.DataAccess
         /// <returns>information sur le prix, Id inclus</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
+            // Load the textfile
+            // Convert the file to a List<PrizeModel>
+            // Find the ID
+            // Add the new record with the new ID (max + 1)
+            // Convert the Prizes to a list<string>
+            // save the list<string> to a file
             model.Id = 1;
             return model;
         }
---
+++ TrackerLib/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLib.Models;
+
+namespace TrackerLib.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(this string fileName) //csv
+        {
+            return $" { ConfigurationManager.AppSettings["filePath"]}\\{ fileName } ";
+        }
+
+        // Load the textfile
+        // Convert the file to a List<PrizeModel>
+        // Find the ID
+        // Add the new record with the new ID (max + 1)
+        // Convert the Prizes to a list<string>
+        // save the list<string> to a file
+        public static List<string> loadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(file).ToList();
+        }
+        public static List<PrizeModel> convertPrizeToModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+            foreach(string line in lines)
+            {
+                string[] cols = line.Split(',');
+                PrizeModel p = new PrizeModel
+                {
+                    Id = int.Parse(cols[0]),
+                    PlaceNumber = int.Parse(cols[1]),
+                    PlaceName = cols[2],
+                    PrizeAmount = decimal.Parse(cols[3]),
+                    PrizePercentage = double.Parse(cols[4])
+                };
+                output.Add(p);
+            }
+
+            return output;
+        }
+    }
+}
